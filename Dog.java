e596cd7464b7433978722919e25f2e9d3442504c
Balaiah <thupakulabalaiah352@gmail.com>
2024-12-28 19:37:19 +0530

    new

---
+++ Dog.java
@@ -0,0 +1,18 @@
+ class Dog {
+	String name;
+	String bread;
+	int cost;
+	public static void main(String[] args) {
+		Dog d = new Dog();
+		d.name="chimtu";
+		d.bread="husky";
+		d.cost=100000;
+		System.out.println(d.name);
+		System.out.println(d.bread);
+		System.out.println(d.cost);
+		
+		
+		
+	}
+
+}
