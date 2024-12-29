1a06e30c8d7919baaf108320f34b62029780d0fd
Balaiah <thupakulabalaiah352@gmail.com>
2024-12-29 19:01:42 +0530

    new

---
+++ Add.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Add {
+public static void main(String[] args) {
+	Scanner s= new Scanner(System.in);
+	int a, b;
+	a=s.nextInt();
+	b=s.nextInt();
+	
+	
+	for(int i =0;i<b;i++) {
+		a++;
+		
+	}
+	System.out.println(a);
+}
+}
