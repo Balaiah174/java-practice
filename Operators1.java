d6a733807a9cc7e073b37b3c9c871d89fcd10891
Balaiah <thupakulabalaiah352@gmail.com>
2024-12-30 22:28:13 +0530

    new

---
+++ Operators1.java
@@ -0,0 +1,11 @@
+public class Operators1 {
+public static void main(String[] args) {
+	int ex=300;
+	System.out.println(ex+=80);
+	System.out.println(ex-=100);
+	System.out.println(ex*=10);
+System.out.println(ex/=10);
+System.out.println(ex%=3);
+
+}
+}
