3f2dedd66c23abce1f07b5fa4183b74cccb40ac1
Balaiah <thupakulabalaiah352@gmail.com>
2024-12-30 22:47:18 +0530

    new

---
+++ balaiah.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class balaiah {
+public static void main(String[] args) {
+	
+	Scanner s=new Scanner (System.in);
+	System.out.println("enter the no of balaiah : ");
+	int n=s.nextInt();
+	for(int i=0;i<n;i++) {
+		
+	for( int j=0;j<n;j++) {
+		System.out.print(i+"    ");
+	}
+	System.out.println();
+
+}
+}
+}
