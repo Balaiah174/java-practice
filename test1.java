26e6b0e9754c3e7790378c5e2773896f8ee296e1
Balaiah <thupakulabalaiah352@gmail.com>
2024-12-30 22:37:46 +0530

    new

---
+++ test1.java
@@ -0,0 +1,14 @@
+public class test1 {
+public static void main(String[] args) {
+	int a= 20;
+	int b= 40;
+	if(a<b)
+	{
+		System.out.println("a is less than b");
+	}
+	else
+	{
+		System.out.println("a is greater than b");
+	}
+}
+}
