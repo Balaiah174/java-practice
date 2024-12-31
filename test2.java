9acc59efb120ee695053a22debf2eda2b6c4a74e
Balaiah <thupakulabalaiah352@gmail.com>
2024-12-31 18:10:32 +0530

    new

---
+++ test2.java
@@ -0,0 +1,14 @@
+
+public class test2 {
+	public static void main(String[] args) {
+		int age=25;
+		if(age >21)
+		{
+			System.out.println("  eligible for marraige");
+		}
+		else
+		{
+			System.out.println("not eligible for marraige");
+		}
+	}
+	}
