ae28f11481e46a067f9a2517a5bfe19a357d0385
Balaiah <thupakulabalaiah352@gmail.com>
2024-12-31 18:05:17 +0530

    new

---
+++ vehicle.java
@@ -0,0 +1,18 @@
+
+public class vehicle {
+String name;
+double price;
+{
+	name="THAR";
+	price=12365598;
+}
+public static void main(String[] args) {
+	vehicle v=new vehicle();
+	System.out.println(v.name);
+	System.out.println(v.price);
+	vehicle v1=new vehicle();
+	System.out.println(v1.name);
+	System.out.println(v1.price);
+	
+}
+}
