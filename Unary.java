47e3bc8a1cb7a263f95ca3d65af80a74ca76fea3
Balaiah <thupakulabalaiah352@gmail.com>
2024-12-31 18:01:57 +0530

    new

---
+++ Unary.java
@@ -0,0 +1,18 @@
+public class Unary {
+public static void main(String[] args) {
+	int a=12;
+	
+			System.out.println(+a);
+
+			System.out.println(-a);
+
+			System.out.println(a++);
+
+			System.out.println(++a);
+
+			System.out.println(a--);
+
+			System.out.println(--a);	
+	
+}
+}
