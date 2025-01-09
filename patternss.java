c889341607702b73e0cc8b9fb99d24cc94fa6d21
Balaiah <thupakulabalaiah352@gmail.com>
2025-01-09 19:21:24 +0530

    new

---
+++ patternss.java
@@ -0,0 +1,38 @@
+
+
+public class patternss {
+	public static void main(String[] args) {
+		
+		 
+		int rows =5;
+		for(int i=1;i<=rows;i++){
+			
+			for (int j=1;j<=(rows-i);j++) {
+				System.out.print(" "+" ");
+			}
+			for (int j=1;j<=i;j++) {
+				System.out.print(i+" ");
+			
+			
+			}
+			for(int j=1;j<=(i-1);j++) {
+				System.out.print(i+" ");
+			}
+			System.out.println();
+		}
+		for(int i=1;i<rows;i++) {
+			for (int j=1;j<=i;j++) {
+				System.out.print(" "+" ");
+			}
+			for (int j=1;j<=(rows-i);j++) {
+				System.out.print("*"+" ");
+			}
+			for (int j=1; j<=rows-i-1;j++) {
+				System.out.print("*"+" ");
+			}
+			System.out.println();
+		}
+
+	} 
+	}
+
