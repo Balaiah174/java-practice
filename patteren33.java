83c1c9903873d933308e34b7da8322ec25c9cee1
Balaiah <thupakulabalaiah352@gmail.com>
2025-01-13 19:32:17 +0530

    new

---
+++ patteren33.java
@@ -0,0 +1,21 @@
+
+
+ class patteren33 {
+	public static void main(String[] args) {
+	
+int rows =5;
+int k=  65;
+for(int i=1;i<=7;i++) {
+	for( int j=1;j<=(7-i);j++) {
+		System.out.print( ""+" ");
+	}
+	char c= (char)k;
+	for(int j=1;j<=i;j++) {
+		System.out.print(j+" ");
+	c--;
+	}
+	System.out.println();
+	k++;
+}
+}
+}    
