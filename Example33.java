5b3656d421e355c510f33e8c1798c0464165222d
Balaiah <thupakulabalaiah352@gmail.com>
2025-01-16 19:55:46 +0530

    new

---
+++ Example33.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+class Example33 {
+public static void main(String[] args) {
+	int a,b;
+	Scanner s = new Scanner(System.in);
+	System.out.println("enter the values for a,b");
+	a= s.nextInt();
+	b=s.nextInt();
+	if(a>b)
+	{
+		System.out.println(a+ "a is big");
+	}
+	else
+	{
+		System.out.println(b+ "b is big");
+	}
+}
+}
