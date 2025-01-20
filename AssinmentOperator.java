1bb6087ef64c74a5ecb814dafb02567e82ea566d
Balaiah <thupakulabalaiah352@gmail.com>
2025-01-20 21:02:12 +0530

    new

---
+++ AssinmentOperator.java
@@ -0,0 +1,21 @@
+
+ class AssinmentOperator {
+	public static void main(String[] args) {
+		int a = 10;
+// add
+		a += 10;
+        System.out.println(a);
+        //sub
+        a-=9;
+		System.out.println(a);
+		// mul
+		a*=5;
+		System.out.println(a);
+		//div 
+		a/=3;
+		System.out.println(a);
+		//mod 
+		a%=6;
+		System.out.println(a);
+	}
+}
