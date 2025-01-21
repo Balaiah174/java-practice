1e488c318de212064489fc69d55adcc4a07581c8
Balaiah <thupakulabalaiah352@gmail.com>
2025-01-21 21:20:24 +0530

    new

---
+++ logicaloperators.java
@@ -0,0 +1,12 @@
+class logicaloperators {
+public static void main(String[] args) {
+	int a=5;
+	//int x=a++ + ++a;
+	System.out.println(a++);
+	System.out.println(a);
+	/*
+	 * if((a<b)&&(a==c)) { System.out.println(true); }
+	 */
+}
+
+}
