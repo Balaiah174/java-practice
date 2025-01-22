ad689f55cabc66b40175ed038f0bc088ab8423f6
Balaiah <thupakulabalaiah352@gmail.com>
2025-01-22 17:24:50 +0530

    new

---
+++ patterns3.java
@@ -0,0 +1,13 @@
+class patterns3 {
+public static void main(String[] args) {
+	int rows=5;
+	char a=65;
+	for(int i=1;i<=5;i++) {
+		for(int j=1;j<=5;j++) {
+			System.out.print(a+" ");
+		}
+		a++;
+		System.out.println();
+	}
+}
+}
