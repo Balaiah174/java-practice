3412a6e07ecb2b1d193a6d25849951cd542c985b
Balaiah <thupakulabalaiah352@gmail.com>
2025-01-23 17:27:32 +0530

    new

---
+++ alphabet.java
@@ -0,0 +1,14 @@
+class alphabet {
+	public static void main(String[] args) {
+		int rows=5;
+char a=97;
+		for(int i=1;i<=5;i++) {
+			for(int j=1;j<=i;j++) {
+				System.out.print(a +" ");
+			}
+		a++;
+			System.out.println();
+		}
+	}
+}
+
