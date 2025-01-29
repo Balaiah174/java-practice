98a31fe46f8717f8b14405008648625b0db3321b
Balaiah <thupakulabalaiah352@gmail.com>
2025-01-29 16:30:23 +0530

    new

---
+++ halfpa1.java
@@ -0,0 +1,17 @@
+ class halfpa1 {
+public static void main(String[] args) {
+		
+		
+		int rows = 5;
+		
+		for (int i = 1; i <= 5; i++) {
+			char a='A';
+			for (int j = i; j <=5; j++) {
+				System.out.print(a);
+				a++;
+				
+			}
+			System.out.println();
+		}
+	}
+}
