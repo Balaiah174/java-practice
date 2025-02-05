450511de9e858461f918d104a5eca52c7fc31ef4
Balaiah <thupakulabalaiah352@gmail.com>
2025-02-05 19:47:29 +0530

    new

---
+++ sum.java
@@ -0,0 +1,11 @@
+class sum {
+public static void main(String[] args) {
+	int start =10;
+	int end =40;
+	int sum =0;
+	for (int i =start;i<=end;i++) {
+		sum+=i;
+	}
+	System.out.println(sum);
+}
+}
