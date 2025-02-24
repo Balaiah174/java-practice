5db2e1ca9eda93f17631a5fa61c85648e270fce9
Balaiah <thupakulabalaiah352@gmail.com>
2025-02-24 22:37:49 +0530

    new

--- Armstrong.java
+++ Armstrong.java
@@ -15,7 +15,31 @@ import java.util.Scanner;
 
 	}
 
-		public static void main(String[] args) {
+	public static int power(int base, int exponent) {
+		int prod = 1;
+		for (int i = 0; i < exponent; i++) {
+			prod = prod * base;
+		}
+		return prod;
+	}
+
+	public static boolean isArmstrong(int num) {
+		int exponent = countdigits(num);
+		int temp = num;
+		int sum = 0;
+		while (num > 0) {
+			int mod = num % 10;
+			sum = sum + power(mod, exponent);
+			num = num / 10;
+		}
+		if (temp == sum) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public static void main(String[] args) {
            Scanner sc=new Scanner(System.in);
            System.out.println("Enter a number");
            int number=sc.nextInt();
