a747d278b83005d00ce0345c958095d47be20e12
Balaiah <thupakulabalaiah352@gmail.com>
2025-02-27 20:51:24 +0530

    new

---
+++ half.java
@@ -0,0 +1,11 @@
+public class half {
+	public static void main(String[] args) {
+		int rows = 5;
+		for (int i = 1; i <= 5; i++) {
+			for (int j = i; j <= 5; j++) {
+				System.out.print((j % 2) + " ");
+			}
+			System.out.println();
+		}
+	}
+}
