c7161f29e28be4ef7f8b4587906af0e4a7403957
Balaiah <thupakulabalaiah352@gmail.com>
2025-03-05 19:17:34 +0530

    new

---
+++ reverserecurssionnumber.java
@@ -0,0 +1,11 @@
+public class reverserecurssionnumber {
+public static  int rev(int num,int rev) {
+	if(num==0)
+		return rev;
+	return rev(num/10,rev*10+num%10);
+	
+}
+public static void main(String[] args) {
+	System.out.println(rev(12345,0));
+}
+}
